41dc1db91931f3a20f21c4048d689ead2c0f8ff9
Marie Godderis <115651524+mariegodderis1@users.noreply.github.com>
2023-04-18 08:54:34 -0700

    Create readme.m

---
+++ a3/readme.m
@@ -0,0 +1,7 @@
+## Assignment 03: User Research
+
+### Description of a project design challenge, target users, research method
+
+For our user testing, we focused on the app [BeReal](https://bere.al/en). Our participant completed the study with this [UT Test Survey](https://forms.gle/9XyNxXfXLqorKahK8). Here is the [link to pilot UT video](https://drive.google.com/file/d/1SnP3w4QwoUsD4dOG2XM0b796JEVeguar/view?usp=sharing) and here is the [screen recording of the user's phone](https://drive.google.com/file/d/1ahrGWD4k7ksoBcH72_H0kqSD1FuF7jaH/view?usp=sharing). Our [research guide and interview notes are here](https://docs.google.com/document/d/1uYXBi7uf4Uf8gIlBGfap03EpEge6lUoB7bC_aaYL3Fc/edit?usp=sharing).
+
+### Reflection
